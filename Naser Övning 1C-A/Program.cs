161b7aec14b1520cb36bfc3fd73ce4a869db5254
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:16:45 +0100

    Add project files.

---
+++ Naser Övning 1C-A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift_5_1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+          string[] Stränger = new string[4];
+            Stränger[0] = "förstsa mening";
+
+            Stränger[1] = "andra mening";
+
+            Stränger[2] = "Tedje mening";
+
+            Stränger[3] = "Fjärde mening";
+
+            for (int i = 0; i < Stränger.Length; i++)
+            {
+                Console.WriteLine(Stränger[i]+"\n"); 
+            }    
+
+
+        }
+
+    }
+
+}
